597b23c10563a7aad22e2157a016b358216553b0
wxwwayne <wxwwayne@gmail.com>
2015-08-22 15:44:57 -0400

    finishi layouts and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,10 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_response :success
     assert_select "title", "About|Faketwitter App"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact|Faketwitter App"
+  end
 end

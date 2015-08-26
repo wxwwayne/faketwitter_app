de50b9dc3544a8913cdc375b64d693a70686fcd9
wxwwayne <wxwwayne@gmail.com>
2015-08-26 15:55:34 -0400

    finishi login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		#deal with successful sign up
+      log_in @user
   		flash[:success] = "Welcome to the Faketwitter APP!"
   		redirect_to @user
   	else

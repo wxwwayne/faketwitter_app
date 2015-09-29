2ed13a2d01c34195ffde5e0a6f61b5cc5e3f8574
wxwwayne <wxwwayne@gmail.com>
2015-09-29 15:57:20 -0400

    Finish some refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #get '/' => 'static_pages#home'
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,7 +8,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   	assert_select 'a[href=?]', help_path
   	assert_select 'a[href=?]', about_path
   	assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', 'http://www.twitter.com', text: "Twitter"
-  	assert_select "div"
+    get signup_path
+    assert_template 'users/new'
+    assert_select "title", full_title("Sign up")#"Sign up|#{base_title}"
+  	assert_select "div.container" #"faketwitter app"
   end
 end

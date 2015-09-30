68f59ea3d7bd7f460fae91b501baefb382bea1e1
wxwwayne <wxwwayne@gmail.com>
2015-09-30 11:59:58 -0400

    relocate helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
 		end
 	end
 
+	# 返回指定用户的Gravatar
+	def gravatar_for(user, options = { size: 80 })
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		size = options[:size]
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
+
+	#It is originally located in users_helper.rb. But I put it here for 
+	#easy maintance!
+	#默认情况下,所有辅助方法文件中定义的方法都自动在任意视图中可用!
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,3 @@
 module UsersHelper
-	# 返回指定用户的Gravatar
-	def gravatar_for(user, options = { size: 80 })
-		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		size = options[:size]
-		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-		image_tag(gravatar_url, alt: user.name, class: "gravatar")
-	end
 
 end

18496ffb4a949f2dff2b203db44ea8fa8346fa71
Xiaowei <wxwwayne@gmail.com>
2015-11-21 13:37:46 -0500

    auth_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
     foreign_key: "followed_id",
     dependent: :destroy
   has_many :followers, through: :passive_relationships
-  attr_accessor :remember_token, :activation_token, :reset_token, :authentication_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
